3d9781e6497e4260eacfd80d9c75138b31e6ed74
Mauricio <kthemaus@gmail.com>
2019-04-24 12:55:25 -0500

    Modificaion de modulos y rutas

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from "@angular/core";
+/// modulod
+import { SharedModule } from '../shared/shared.module';
+import { PAGES_ROUTES } from "./pages.routes";
+
+import { PagesComponent } from "./pages.component";
+
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { ProgressComponent } from "./progress/progress.component";
+import { Grafica1Component } from "./grafica1/grafica1.component";
+
+
+@NgModule({
+    declarations: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Grafica1Component
+    ],
+    exports: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Grafica1Component
+    ],
+    imports: [
+        SharedModule,
+        PAGES_ROUTES
+    ]
+})
+
+export class PagesModule {
+
+}
